90f90574cde4d493128735fc71904e63e97162fb
ngockhoi96 <anicedfaant@gmail.com>
2025-03-03 22:29:49 +0700

    :rocket: deploy(next,package): change to node server deploy to vercel

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const buildENV = process.env.BUILD_ENV || 'production';
 console.log(`BUILD_ENV: ${buildENV}, NODE_ENV: ${process.env.NODE_ENV}`);
 
 const nextConfig: NextConfig = {
-  // Recommended: this will reduce output
-  // Docker image size by 80%+
-  output: 'standalone',
   // Optional: bring your own cache handler
   // cacheHandler: path.resolve('./cache-handler.mjs'),
   // cacheMaxMemorySize: 0, // Disable default in-memory caching
@@ -30,25 +27,6 @@ const nextConfig: NextConfig = {
         search: '',
       },
     ],
-  },
-  // Nginx will do gzip compression. We disable
-  // compression here so we can prevent buffering
-  // streaming responses
-  compress: false,
-  // Optional: override the default (1 year) `stale-while-revalidate`
-  // header time for static pages
-  // swrDelta: 3600 // seconds,
-  eslint: {
-    // Warning: This allows production builds to complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: buildENV === 'production' ? true : false,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to complete
-    // even if your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: buildENV === 'production' ? true : false,
   },
 };
 
